7abe55cd42e76650f57d5dcbee072bc7760c58f5
chaitanya <mogallachaitanyasaiphanidhar@gmail.com>
2026-03-31 12:36:10 +0530

    image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <div className="max-w-5xl mx-auto mb-10 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <img 
-  src="/phoenix.png" 
+  src="/pheonix.png" 
   alt="Phoenix Logo" 
   className="w-8 h-8 object-contain"
 />
