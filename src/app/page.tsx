49108507abc43fca8c6e693ca6b7126d3171e89a
chaitanya <mogallachaitanyasaiphanidhar@gmail.com>
2026-03-31 12:31:21 +0530

    added image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,11 @@ export default function Home() {
       {/* HEADER */}
       <div className="max-w-5xl mx-auto mb-10 flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className="text-2xl">🔥</div>
+          <img 
+  src="/phoenix.png" 
+  alt="Phoenix Logo" 
+  className="w-8 h-8 object-contain"
+/>
           <h1 className="text-2xl font-bold tracking-wide">
             MOZEN
           </h1>
